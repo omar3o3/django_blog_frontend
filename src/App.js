6fd9521064f72f9022415947b6b49dd0bd43ca4d
omar3o3 <otantawy53@gmail.com>
2022-10-27 14:57:58 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState, useEffect} from 'react'
 
 function App() {
+
+  const [django, setDjango] = useState([]);
+
+  useEffect(() => {
+    fetch("/users/get_users")
+      .then((resp) => resp.json())
+      .then((data) => setDjango(data));
+  }, []);
+
+  console.log(django);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
